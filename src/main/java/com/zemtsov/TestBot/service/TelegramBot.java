30445ea2575d5850105a493872f9ce3671c5aeae
Evgenii Zemtsov <sever-zema@mail.ru>
2022-07-16 21:35:40 +0300

    Initial commit

---
+++ src/main/java/com/zemtsov/TestBot/service/TelegramBot.java
@@ -0,0 +1,68 @@
+package com.zemtsov.TestBot.service;
+
+import com.zemtsov.TestBot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandAction(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, I don't understand what you're saying\nTry to send /start");
+            }
+
+        }
+
+    }
+
+    private void startCommandAction(long chatId, String name) {
+
+        String answer = "Hello, " + name + "!!!\n" +
+                "How are you doing?\n" +
+                "I'm glad to see you here!!!";
+
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+}
---
+++ src/test/java/com/zemtsov/TestBot/TestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zemtsov.TestBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
