7e96efc93e0826e8ed4cdebfc8d05d227927a158
Evgenii Zemtsov <sever-zema@mail.ru>
2022-07-18 17:46:33 +0300

    Initial adding bot commands

--- src/main/java/com/zemtsov/TestBot/service/TelegramBot.java
+++ src/main/java/com/zemtsov/TestBot/service/TelegramBot.java
@@ -4,18 +4,35 @@ import com.zemtsov.TestBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
-    BotConfig config;
+    final BotConfig config;
+    private final String HELP_MESSAGE = "This is my firs Telegram bot, created to learn how to work with Telegram API\n\n" +
+            "You can execute the following commands by choosing them from menu or by typing them: \n\n" +
+            "/start for getting a welcome message\n\n" +
+            "/mydata for checking you personal data\n\n" +
+            "/deletedata for deleting your data\n\n" +
+            "/help for checking the help desk\n\n" ;
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        try {
+            this.execute(new SetMyCommands(addBotCommands(), new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error occurred while adding menu: " + e.getMessage());
+        }
     }
 
     @Override
@@ -41,6 +58,10 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/start":
                     startCommandAction(chatId, update.getMessage().getChat().getFirstName());
                     log.info("Executed command /start");
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_MESSAGE);
+                    log.info("Executed command /help");
                     break;
                 default:
                     sendMessage(chatId, "Sorry, I don't understand what you're saying\nTry to send /start");
@@ -70,5 +91,16 @@ public class TelegramBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             log.error("The error occurred while sending message: " + e.getMessage());
         }
+    }
+
+    private List<BotCommand> addBotCommands() {
+        List<BotCommand> listOfCommands = new ArrayList<>();
+
+        listOfCommands.add(new BotCommand("/start", "get welcome message"));
+        listOfCommands.add(new BotCommand("/mydata", "get my data"));
+        listOfCommands.add(new BotCommand("/deletedata", "deleting user data"));
+        listOfCommands.add(new BotCommand("/help", "get help message"));
+
+        return listOfCommands;
     }
 }
